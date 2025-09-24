fdd7fc78f207be89efec6aa23f2777d90a22118c
nelfen <nelfen.dev@gmail.com>
2025-09-24 11:08:30 +0900

    ES6문법과 common.js 문법의 차이

--- multiply.js
+++ multiply.js
@@ -1,3 +1,4 @@
 const multiply = (x, y) => console.log(`${x} x ${y} = ${x * y}`);
 
+// module.exports = multiply;
 export default multiply;
--- two.js
+++ two.js
@@ -1,3 +1,4 @@
+// const multiply = require("./multiply.js");
 import multiply from "./multiply.js";
 
 multiply(2, 1);
