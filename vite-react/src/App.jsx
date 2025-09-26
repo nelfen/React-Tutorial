2d25cc666b04d6949cebaba44dd64c6ee9089479
nelfen <nelfen.dev@gmail.com>
2025-09-26 23:07:31 +0900

    State 사용법

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -19,8 +19,8 @@ class App extends Component {
     return (
       <>
         <div>counter: {this.state.counter}</div>
-        <button onClick={() => this.setState({ counter: this.state.counter +  1 })}>+</button>
-        <button onClick={() => this.setState({ counter: this.state.counter -  1 })}>-</button>
+        <button onClick={() => this.setState((state) => ({ counter: state.counter + 1 }))}>+</button>
+        <button onClick={() => this.setState((state) => ({ counter: state.counter - 1 }))}>-</button>
       </>
     );
   }
