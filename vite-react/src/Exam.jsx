ead963eeed15dae93f50bacc84adbc0ba540f54f
nelfen <nelfen.dev@gmail.com>
2025-09-27 23:29:19 +0900

    State 예제

---
+++ vite-react/src/Exam.jsx
@@ -0,0 +1,14 @@
+class App extends Component {
+  state = { counter: 0 }
+  render() {
+    return (
+      <>
+        <div>counter: {this.state.counter}</div>
+        <button onClick={() => this.setState((state) => ({ counter: state.counter + 1 }))}>+</button>
+        <button onClick={() => this.setState((state) => ({ counter: state.counter - 1 }))}>-</button>
+      </>
+    );
+  }
+}
+
+// export default App
