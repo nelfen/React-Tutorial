4992d3b4567bc427f5da05712e220bb441eb544f
nelfen <nelfen.dev@gmail.com>
2025-10-02 18:18:26 +0900

    ToDoList 완성

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -6,19 +6,64 @@ function App() {
     { id: 0, content: "밥먹기" },
     { id: 1, content: "코딩 공부하기" },
   ]);
+
   return (
     <>
-      <TodoList todoList={todoList} />
+      <TodoList todoList={todoList} setTodoList={setTodoList} />
+      <hr />
+      <TodoInput todoList={todoList} setTodoList={setTodoList} />
     </>
   );
 }
 
-function TodoList({ todoList }) {
-  <ul>
-    {todoList.map((todo) => (
-      <li key={todo.id}>{todo.content}</li>
-    ))}
-  </ul>;
+function TodoInput({ todoList, setTodoList }) {
+  const [inputValue, setInputValue] = useState("");
+  return (
+    <>
+      <input
+        value={inputValue}
+        onChange={(event) => setInputValue(event.target.value)}
+      />
+      <button
+        onClick={() => {
+          const newTodo = { id: Number(new Date()), content: inputValue };
+
+          const newTodoList = [...todoList, newTodo];
+          setTodoList(newTodoList);
+          setInputValue("");
+        }}
+      >
+        추가하기
+      </button>
+    </>
+  );
+}
+
+function TodoList({ todoList, setTodoList }) {
+  return (
+    <ul>
+      {todoList.map((todo) => (
+        <Todo key={todo.id} todo={todo} setTodoList={setTodoList} />
+      ))}
+    </ul>
+  );
+}
+
+function Todo({ todo, setTodoList }) {
+  return (
+    <li>
+      {todo.content}
+      <button
+        onClick={() => {
+          setTodoList((prev) => {
+            return prev.filter((el) => el.id !== todo.id);
+          });
+        }}
+      >
+        삭제
+      </button>
+    </li>
+  );
 }
 
 export default App;
