88ba8f95e8cfd6d5424de7e23efed9ea635884f9
nelfen <nelfen.dev@gmail.com>
2025-10-03 23:03:59 +0900

    TodoList 기능추가

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -2,10 +2,7 @@ import { Component, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [todoList, setTodoList] = useState([
-    { id: 0, content: "밥먹기" },
-    { id: 1, content: "코딩 공부하기" },
-  ]);
+  const [todoList, setTodoList] = useState([]);
 
   return (
     <>
