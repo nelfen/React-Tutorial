ab597ba42b16a3a08bd11fbb21f5864fa39c0795
nelfen <nelfen.dev@gmail.com>
2025-10-13 16:27:22 +0900

    컴포넌트 사용해보기

---
+++ vite-react/src/02_ComponentNesting.jsx
@@ -0,0 +1,50 @@
+function Menu() {
+  return (
+    <>
+      <Burger />
+    </>
+  );
+}
+
+function Burger() {
+  return (
+    <>
+      <Bread />
+      <Beef />
+      <Leap />
+      <Bread />
+    </>
+  );
+}
+
+function Bread() {
+  return (
+    <div className="bread">
+      밀가루
+      <span> 물</span>
+      <span> 계란</span>
+      <span> 이스트</span>
+    </div>
+  );
+}
+
+function Beef() {
+  return (
+    <div className="beef">
+      소고기
+      <span> 후추</span>
+      <span> 소금</span>
+    </div>
+  );
+}
+
+function Leap() {
+  return (
+    <div className="leap">
+      잎사귀
+      <span> 줄기</span>
+    </div>
+  );
+}
+
+export default Menu;
