f895c5ed03bf04aaf40de27d0ddf2ea693edfb71
nelfen <nelfen.dev@gmail.com>
2025-10-16 18:03:51 +0900

    함수 컴포넌트의 생명주기

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -1,4 +1,4 @@
-import { Component, useState } from "react";
+import { Component, useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
@@ -7,43 +7,49 @@ function App() {
   return (
     <>
       {showCounter && <Counter />}
-      <button onClick={() => setShowCounter(prev => !prev)}>show?</button>
+      <button onClick={() => setShowCounter((prev) => !prev)}>show?</button>
     </>
   );
 }
 
-class Counter extends Component {
-  constructor() {
-    super();
-    this.state = { counter: 1 };
-    console.log("constructor");
-  }
+function Counter() {
+  const [counter, setCounter] = useState(1);
+  const [counter2, setCounter2] = useState(100);
 
-  componentDidMount() {
-    console.log("DidMount");
-  }
+  // 1. 컴포넌트가 최초로 렌더링 될 때에만 조작을 하고싶다!
+  useEffect(() => {
+    console.log("맨 처음 렌더링 될 때");
+  }, []);
 
-  componentDidUpdate() {
-    console.log("DidUpdate");
-  }
+    // 2. 컴포넌트가 렌더링 될 때 조작하고 싶다!
+  useEffect(() => {
+    console.log("리렌더링..");
+  });
 
-  componentWillUnmount() {
-    console.log("WillUnmount");
-  }
+  // 3. 특정 상태값이 변할 때에만 조작하고 싶다!
+  useEffect(() => {
+    console.log("counter의 값이 변할 때");
+  }, [counter]);
 
-  render() {
-    console.log("render");
-    return (
-      <>
-        <div>counter : {this.state.counter}</div>
-        <button
-          onClick={() => this.setState({ counter: this.state.counter + 1 })}
-        >
-          +1
-        </button>
-      </>
-    );
-  }
+  useEffect(() => {
+    console.log("counter2의 값이 변할 때");
+  }, [counter2]);
+
+  // 4. 컴포넌트가 최종적으로 언마운트 될 때 조작하고 싶다!
+  useEffect(() => {
+    return () => {
+      console.log("컴포넌트 언마운트");
+    }
+  }, []);
+
+  return (
+    <>
+      <div>counter : {counter}</div>
+      <button onClick={() => setCounter(counter + 1)}>+1</button>
+      <div>counter2 : {counter2}</div>
+      <button onClick={() => setCounter2(counter2 - 1)}>-1</button>
+    </>
+  );
 }
 
 export default App;
