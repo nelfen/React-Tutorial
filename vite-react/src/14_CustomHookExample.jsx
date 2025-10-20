da10bc940be28ddb2dfe50b67bb3b9407cc24857
nelfen <nelfen.dev@gmail.com>
2025-10-20 16:37:03 +0900

    커스텀 Hook 기초 예제

---
+++ vite-react/src/14_CustomHookExample.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+export default function CustomHookExample() {
+  const {count, increment, decrement} = useCounter(0);
+
+  return (
+    <div>
+      <div>count: {count}</div>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+    </div>
+  );
+}
+
+const useCounter = (initialValue = 0) => {
+  const [count, setCount] = useState(initialValue);
+
+  const increment = () => setCount((prev) => prev + 1);
+  const decrement = () => setCount((prev) => prev - 1);
+
+  return {count, increment, decrement}
+}
