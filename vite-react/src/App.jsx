8fd073e33d5e643a87dc78d9fa32255074213c49
nelfen <nelfen.dev@gmail.com>
2025-10-21 16:25:40 +0900

    중첩

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -6,10 +6,20 @@ function App() {
       <div className='test'>테스트용 div 입니다</div>
       <h2>SCSS 변수 연습</h2>
       <ul>
-        <li>첫째줄</li>
-        <li>둘째줄</li>
-        <li>셋째줄</li>
-        <li>넷째줄</li>
+        <li>첫째줄
+          <p>P태그요소</p>
+        </li>
+        <li>둘째줄
+          <section>Section태그요소</section>
+        </li>
+        <li>셋째줄
+          <article>Article태그요소
+            <h3>h3태그요소</h3>
+          </article>
+        </li>
+        <li>넷째줄
+          <h2>h2태그요소</h2>
+        </li>
       </ul>
     </>
   );
