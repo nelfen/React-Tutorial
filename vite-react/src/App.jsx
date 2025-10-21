f21bfc62ce02050dd42709b654d727a2ffbf8a47
nelfen <nelfen.dev@gmail.com>
2025-10-21 15:00:39 +0900

    sass 셋팅

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -1,19 +1,34 @@
-import React, { useState } from 'react';
+import { useRef, useState } from 'react';
+import './App.css';
 
 function App() {
-  const [person, setPerson] = useState({ name: 'John', age: 25 });
+  // 검색어: 상태(state)로 관리 -> 사용자에게 즉각적으로 UI를 보여주기 위해
+  const [searchText, setSearchText] = useState('');
+  const searchContent = useRef(null);
+  // 1. input창에 접근하기 위한 useRef를 선언하세요.
 
-  const handleClick = () => {
-    setPerson({ ...person, age: person.age + 1 }); // 상태를 직접 변경
-    console.log(person); // 콘솔에서는 변경된 값이 보이지만, 화면에는 반영되지 않음
+  // 2. input창에서 검색 단어를 받아와 상태를 업데이트 하는 함수를 작성하세요.
+  const getSearchValue = () => {
+    const inputValue = searchContent.current.value; 
+    setSearchText(inputValue);
   };
 
   return (
-    <div>
-      <p>Name: {person.name}</p>
-      <p>Age: {person.age}</p>
-      <button onClick={handleClick}>Increase Age</button>
-    </div>
+    <>
+      <h2>OZ 코딩스쿨</h2>
+      <p>검색할 키워드를 입력한 후 검색 버튼을 클릭하세요.</p>
+      <div className="search-input">
+        {/* 3. 선언한 useRef와 input을 연결하세요. */}
+        <input ref={searchContent} />
+        {/* 4. 검색 버튼을 클릭하면 검색어의 상태가 변경되도록 하세요.*/}
+        <button onClick={getSearchValue}>검색</button>
+      </div>
+      {searchText && (
+        <div>
+          🔎 <b>{searchText}</b> 에 대해 검색합니다 . . .
+        </div>
+      )}
+    </>
   );
 }
 
