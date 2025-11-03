8919908ba5f1d20d68dfdd42a90e9e1ffeaea53f
nelfen <nelfen.dev@gmail.com>
2025-11-04 00:44:54 +0900

    SCSS 시작

---
+++ React-Tutorial/src/16_FirstScss.jsx
@@ -0,0 +1,16 @@
+import './16_FirstScss.scss';
+
+export default function FirstScss() {
+  return (
+    <>
+      <div className='test'>테스트용 div 입니다</div>
+      <h2>SCSS 변수 연습</h2>
+      <ul>
+        <li>첫째줄</li>
+        <li>둘째줄</li>
+        <li>셋째줄</li>
+        <li>넷째줄</li>
+      </ul>
+    </>
+  );
+}
