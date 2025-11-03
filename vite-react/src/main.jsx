86504cc19a740a3b690ca5c3ef34ede4c2465e0e
nelfen <nelfen.dev@gmail.com>
2025-11-03 17:41:30 +0900

    TailwindCss 셋팅

--- vite-react/src/main.jsx
+++ vite-react/src/main.jsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import './index.scss'
+// import './inex.scss'
+import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
