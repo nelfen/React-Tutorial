05154d0004bff23c7659dc2174654700e8875018
nelfen <nelfen.dev@gmail.com>
2025-11-04 11:23:15 +0900

    변경사항 저장

--- React-Tutorial/src/App.jsx
+++ React-Tutorial/src/App.jsx
@@ -1,13 +1,9 @@
 import "./App.css";
-import styled, { createGlobalStyle } from "styled-components";
-import { useState } from "react";
 
 function App() {
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+
     </>
   );
 }
