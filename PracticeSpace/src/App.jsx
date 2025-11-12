69ffd9d3c4bbd20ee60943f1ef81f6ea7c50f73a
nelfen <nelfen.dev@gmail.com>
2025-11-12 21:34:58 +0900

    예습

--- PracticeSpace/src/App.jsx
+++ PracticeSpace/src/App.jsx
@@ -3,6 +3,6 @@ import "./App.css";
 import PageLayOut from "./PageLayout";
 
 export default function App() {
-  const movieTitle = "고질라 X 콩";
+  const movieTitle = "쿵푸팬더";
   return <PageLayOut title={movieTitle} />;
 }
