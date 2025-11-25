d67861bdce19b49a6a6bc0a30ef4787736709fd3
nelfen <nelfen.dev@gmail.com>
2025-11-25 18:00:33 +0900

    배열

---
+++ TypeScript-Tutorial/src/01_ArrayType.ts
@@ -0,0 +1,5 @@
+let scores: number[] = [85, 92, 78];
+// scores.push("백점");
+
+let names: Array<string> = ["김철수", "이영희", "박민수"];
+names.push("최지수");
--- TypeScript-Tutorial/src/objEx.ts
+++
@@ -1,5 +0,0 @@
-let a:number = 1;
-
-function s(a:string) {
-
-}
