aa3466452d9326a49504856047dab872d41177a5
nelfen <nelfen.dev@gmail.com>
2025-12-02 11:05:03 +0900

    튜플

---
+++ TypeScript-Tutorial/dist/07_tupleTypes.js
@@ -0,0 +1,8 @@
+let userRole;
+userRole = [101, "Admin", true];
+console.log(`사용자 ID: ${userRole[0]}`);
+console.log(`역할 이름: ${userRole[1]}`);
+console.log(`활성화 상태: ${userRole[2]}`);
+export {};
+// userRole = ["Guest", 200, false]; 
+// userRole = [300, "Manager"];
