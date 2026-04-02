3ab865fb753249a2d3548cc8a97ef09cb1ee2f4b
nelfen <nelfen.dev@gmail.com>
2026-04-02 23:51:38 +0900

    homework

--- TypeScript-Tutorial/src/20_async.ts
+++ TypeScript-Tutorial/src/20_async.ts
@@ -5,18 +5,18 @@ interface Todo {
 }
 
 async function fetchTodo(id: number): Promise<Todo> {
-  await new Promise(resolve => setTimeout(resolve, 1000));
+  await new Promise((resolve) => setTimeout(resolve, 1000));
 
   return {
     id: id,
-    title: "타입스크립트 공부하기",
+    title: "타입스크립트 학습하기",
     completed: false,
   };
 }
 
 async function main() {
   console.log("데이터 불러오는 중...");
-  
+
   try {
     const todo = await fetchTodo(1);
     console.log(`도착한 할 일: ${todo.title}`);
