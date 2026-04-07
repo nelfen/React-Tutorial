f1195d94ec62a3498fdf2be6a602eec85adc9535
nelfen <nelfen.dev@gmail.com>
2026-04-07 23:39:56 +0900

    test

--- TypeScript-Tutorial/src/16_advancedTypes.ts
+++ TypeScript-Tutorial/src/16_advancedTypes.ts
@@ -12,12 +12,11 @@ type ElevatedEmployee = Admin & Employee;
 
 const e1: ElevatedEmployee = {
   name: "김철수",
-  privileges: ["서버 관리", "채용"],
+  privileges: ["마케팅", "채용"],
   startDate: new Date(),
 };
 
 console.log(`${e1.name}님은 오늘부터 근무하며, 권한은 ${e1.privileges}입니다.`);
-
 
 //Discriminated Union
 interface SuccessState {
