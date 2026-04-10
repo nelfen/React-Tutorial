15d9c90937d74bd292c49512a691bb5e01b658dc
nelfen <nelfen.dev@gmail.com>
2026-04-10 23:50:33 +0900

    docs

--- TypeScript-Tutorial/src/08_enumTypes.ts
+++ TypeScript-Tutorial/src/08_enumTypes.ts
@@ -1,17 +1,16 @@
 enum Direction {
-    Up,     // 0
-    Down,   // 1
-    Left,   // 2
-    Right   // 3
+  Up, // 0
+  Down, // 1
+  Left, // 2
+  Right, // 3
 }
 
 function movePlayer(direction: Direction) {
-    if (direction === Direction.Up) {
-        console.log("플레이어가 위로 움직입니다. (값: " + direction + ")");
-    } else {
-        console.log("다른 방향으로 움직입니다.");
-    }
+  if (direction === Direction.Up) {
+    console.log("플레이어가 위로 움직입니다. (값: " + direction + ")");
+  } else {
+    console.log("다른 방향으로 움직입니다.");
+  }
 }
 
 movePlayer(Direction.Up);
-// movePlayer(5);
